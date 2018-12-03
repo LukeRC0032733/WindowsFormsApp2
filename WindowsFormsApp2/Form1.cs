88704344998d02913ed0e8dde671eceb916e2822
Luke Steven John Case <rc0032733@student.riversidecollege.ac.uk>
2018-12-03 15:29:52 +0000

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0e0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00e00") + "\n");
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
+            richTextBox1.AppendText(pi.ToString("N") + "\n");
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N0") + "\n");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
